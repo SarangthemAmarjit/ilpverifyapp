8d2c86fd3327c955b24aeeac6141c2f3a97e577b
SarangthemAmarjit <sarangthemamarjit123@gmail.com>
2024-12-15 09:35:42 +0530

    Updated barcode package updated

--- lib/controller/scancontroller.dart
+++ lib/controller/scancontroller.dart
@@ -3,7 +3,8 @@ import 'dart:developer';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_barcode_scanner_plus/flutter_barcode_scanner_plus.dart';
+
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
